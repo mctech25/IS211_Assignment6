e0a581a5e1e9a870281811d4f7891ba59a19f161
mctech25 <mctech25@aol.com>
2025-03-09 18:52:32 -0400

    Add files via upload

---
+++ conversions.py
@@ -0,0 +1,22 @@
+def convertCelsiusToKelvin(celsius):
+    """Converts Celsius to Kelvin."""
+    return celsius + 273.15
+
+def convertCelsiusToFahrenheit(celsius):
+    """Converts Celsius to Fahrenheit."""
+    return (celsius * 9/5) + 32
+def convertFahrenheitToCelsius(fahrenheit):
+    """Converts Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * 5/9
+
+def convertFahrenheitToKelvin(fahrenheit):
+    """Converts Fahrenheit to Kelvin."""
+    return (fahrenheit + 459.67) * 5/9
+
+def convertKelvinToCelsius(kelvin):
+    """Converts Kelvin to Celsius."""
+    return kelvin - 273.15
+
+def convertKelvinToFahrenheit(kelvin):
+    """Converts Kelvin to Fahrenheit."""
+    return (kelvin * 9/5) - 459.67
